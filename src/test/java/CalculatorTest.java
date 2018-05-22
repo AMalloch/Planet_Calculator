2593034d7e23a159cabcf53265e785a0d64c1fb9
Angus Malloch <angusbass@hotmail.com>
2018-05-22 13:39:58 +0100

    Added method to change orbit from days to seconds in Calculator class. Changed person days on planet to integer. Tested method to change orbit from days to seconds, passing.

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,10 +1,35 @@
 import org.junit.Before;
 import org.junit.Test;
+import models.*;
+import models.Person;
+
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
-    @Before
-    public void setUp() throws Exception {
+    private Person person;
+    private Planet planet;
+    private Calculator calculator;
 
+//    Mercury: Orbital period 0.2408467 Earth years
+//    Venus: Orbital period 0.61519726 Earth years
+//    Mars: Orbital period 1.8808158 Earth years
+//    Jupiter: Orbital period 11.862615 Earth years
+//    Saturn: Orbital period 29.447498 Earth years
+//    Uranus: Orbital period 84.016846 Earth years
+//    Neptune: Orbital period 164.79132 Earth years
+//    And, depending on your feelings towards Pluto...
+//    Pluto: Orbital period 248.00 Earth years
+
+    @Before
+    public void setUp() {
+        person = new Person( "Angus", 71);
+        planet = new Planet( "Mercury", 87.97);
+        calculator = new Calculator("Space Age Calculator", person, planet);
+    }
+
+    @Test
+    public void ChangeOrbitToSeconds() {
+        assertEquals(7600608, calculator.convertOrbitDaysToSeconds(planet), 0.01);
     }
 }
